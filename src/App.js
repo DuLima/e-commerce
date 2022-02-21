106e798b051b320585e99a31d5290d0b4e069257
10000152 <Duarte.Lima@pst.asseco.com>
2022-02-21 16:17:27 +0000

    Implementation of Header Component

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import './App.css';
 // Import Components
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,27 @@
+// Import React Component
+import React from "react";
+import { Link } from "react-router-dom";
+
+// Import SCSS
+import './header.styles.scss'
+
+// Import SVG
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Header = () => (
+    <div className="header">
+        <Link className="logo-container" to='/'>
+            <Logo className="logo"></Logo>
+        </Link>
+        <div className="options">
+            <Link className="option" to='/shop'>
+                SHOP
+            </Link>
+            <Link className="option" to='/shop'>
+                CONTACT
+            </Link>
+        </div>
+    </div>
+)
+
+export default Header
